04e15f2bdab2ecb21f7c69521599e9f29302c32b
Paul Hendry <paul@pshendry.com>
2015-01-13 07:42:37 -0500

    Add filter to skillset table

--- js/script.js
+++ js/script.js
@@ -8,4 +8,24 @@ $("#menu-toggle").click(function(e) {
 $(function () {
   // Initialize Bootstrap tooltips
   $('[data-toggle="tooltip"]').tooltip()
+
+  // Handle filters
+  $(".filterable .filter").click(function() {
+      var filterVal = $(this).text().toLowerCase().replace(' ','-');
+      if (filterVal == "all") {
+        $(this).parents().find(".filterable").find(".filter-item").each(function() {
+          $(this).animate({ opacity: 1 }, 200);
+        });
+      }
+      else {
+        $(this).parents().find(".filterable").find(".filter-item").each(function() {
+          if ($(this).hasClass(filterVal)) {
+            $(this).animate({ opacity: 1 }, 200);
+          }
+          else {
+            $(this).animate({ opacity: 0.25 }, 200);
+          }
+        });
+      }
+  });
 })
